ee8f3b72e2fda115e07923c4b0f7ff1dc70ea69f
Bruno Dias <onurb.braz@gmail.com>
2024-02-03 23:08:31 -0300

    feat: First Commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import HistAtend from './views/HistAtend.vue'
+</script>
+
+<template>
+  <HistAtend />
+</template>
+
+<style scoped></style>
---
+++ src/views/HistAtend.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TabelaHist from '../components/TabelaHist.vue'
+import Pagination from '../components/Pagination.vue'
+import Search from '../components/Search.vue'
+</script>
+
+<template>
+    <div class="container">
+        <div class="flex justify-end my-3">
+            <Search />
+        </div>
+        <div class="mb-3">
+            <TabelaHist />
+        </div>
+        <div>
+            <Pagination />
+        </div>
+    </div>
+</template>
